7df1e322e84bee2f2520cd322c231c5395df2f3d
Eilison98 <in05013300@gmail.com>
2023-06-19 23:29:44 +0900

    2023.06.19

--- StudyTest/List/List_01.py
+++ StudyTest/List/List_01.py
@@ -1,3 +1,16 @@
 # 문자열 리스트
 fruits = ['banana', 'apple', 'orange', 'kiwi']
 print(fruits)
+
+# 리스트 추가
+fruits.append('bin')
+
+# 리스트 삭제
+del fruits[3]
+
+fruits.remove('apple')
+
+# in 연산자
+if ('kiwi' in fruits):
+    fruits.remove('kiwi')
+
